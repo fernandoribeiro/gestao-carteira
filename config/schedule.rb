1eec4a1d6d15752b28effe13458fa7d31a595aba
Guilherme <guilherme@devconnit.com>
2013-10-31 15:31:02 -0200

    Schedule com popula tabelas

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,12 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# whenever --update-crontab store
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
+# every 3.hours do
+#   runner "RelatorioFrequenciaValor.popula_tabela"
 # end
 
-# Learn more: http://github.com/javan/whenever
-
-every 3.hours do
-  runner "RelatorioFrequenciaValor.popula_tabela"
+every 30.minutes do
+	runner "ClienteTemp.popula_tabela_master"
+	runner "ProdutoTemp.popula_tabela_master"
+	runner "VendedorTemp.popula_tabela_master"
+  runner "NotaFiscalTemp.popula_tabela_master"
 end
